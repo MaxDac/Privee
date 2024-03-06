52c190205afc11b70229bd20f06d803936f8e06c
MaxDac <25101798+MaxDac@users.noreply.github.com>
2024-03-06 21:56:12 +0000

    Implement CI tests for Elixir (#4)
    * Create elixir.yml
    
    * Adding CI
    
    * Added devcontainer extensions
    
    * Added tooling ext. to codespaces config
    
    * Fixed Credo
    
    * Updated test postgres password
    
    * Removing dialyzer errors for NIF
    
    * Adding Zig NIFs compilation step
    
    * NIFs with GHA
    
    * Fix
    
    * Added Erlang to the machine
    
    * Added shell to GitHub Actions
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Removing Zig version
    
    * Fixing
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Printing where is Erlang
    
    * Added ERL include folder specification
    
    * Fix
    
    * GH actions refactor
    
    * Fix
    
    * Set Node version
    
    * Fix
    
    * Fix
    
    * Fixed checkout code
    
    * Removing node 16 actions
    
    * Added debugging step for folder
    
    * Fix
    
    ---------
    
    Co-authored-by: MaxDac <git.chaps793@passinbox.com>
    Co-authored-by: MaxDac <mdacunzo@microsoft.com>

--- apps/privee/test/support/data_case.ex
+++ apps/privee/test/support/data_case.ex
@@ -16,6 +16,8 @@ defmodule Privee.DataCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       alias Privee.Repo
@@ -36,8 +38,8 @@ defmodule Privee.DataCase do
   Sets up the sandbox based on the test tags.
   """
   def setup_sandbox(tags) do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Privee.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = Sandbox.start_owner!(Privee.Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
   end
 
   @doc """
