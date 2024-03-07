1aa0b3fb35d2b6e7d5fd2b73ed8aec4afbe8b5dc
MaxDac <25101798+MaxDac@users.noreply.github.com>
2024-03-07 15:56:30 +0000

    Added tooling

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git clone https://codeberg.org/MaxDac/dotfiles.git ~/.dotfiles && \
+sh ~/.dotfiles/setup-environment.sh && \
+mix local.hex --force && \
+mix local.rebar --force && \
+mix archive.install --force hex phx_new && \
+mix deps.get && \
+mix deps.compile && \
+zig build --build-file nifs/build.zig -- /usr/local/lib/erlang/erts-14.2.2/include
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Privee
 Privee FHL project for Microsoft
+
+## How to access in SSH
+
+To access in SSH to develop using NeoVim, follow [these instructions](https://github.com/microsoft/vscode-dev-containers/blob/main/script-library/docs/sshd.md#usage-when-this-script-is-already-installed-in-an-image) in Codespaces.
