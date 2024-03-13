e9b3813c879316b580651a5fdc18fbaf63a3ff4c
MaxDac <25101798+MaxDac@users.noreply.github.com>
2024-03-13 11:08:11 +0000

    #21 Fixed the login page (#27)
    * Tweaked for gh ssh
    
    * Added Fly.io url on Fly.io

--- .devcontainer/install-neovim-tooling.sh
+++ .devcontainer/install-neovim-tooling.sh
@@ -5,7 +5,6 @@ sh ~/.dotfiles/setup-environment.sh && \
 sh ~/.dotfiles/install-neovim-tooling.sh && \
 zsh && \
 asdf plugin add neovim && \
-asdf plugin add elixir && \
-asdf plugin add erlang && \
-asdf plugin add nodejs && \
-asdf plugin add tmux
+asdf plugin add tmux && \
+asdf install neovim && \
+asdf install tmux 
