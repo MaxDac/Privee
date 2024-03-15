414360ec6c07421349c35c1233d37a7ee054cf73
MaxDac <massimiliano.dacunzo@hotmail.com>
2024-03-15 13:13:47 +0000

    Tests failing - Fill mocks

--- apps/privee_web/assets/js/tests/dark-mode-switcher.test.mjs
+++ apps/privee_web/assets/js/tests/dark-mode-switcher.test.mjs
@@ -1,5 +1,18 @@
 import test from "ava"
+import { setStartupTheme } from "../utils/dark-mode-switcher.mjs"
+import { documentMock, localStorageMock, windowMock } from "./mocks.mjs"
+
+test.beforeEach(() => {
+  global.localStorage = localStorageMock()
+  // @ts-ignore
+  global.window = windowMock
+  // @ts-ignore
+  global.document = documentMock
+  // global.localStorage.setItem("color-theme", "light")
+
+})
 
 test("Browser switch to Dark mode when light mode was selected", t => {
-  t.is(2 + 2, 4)
+  setStartupTheme()
+  t.is(global.localStorage.getItem("color-theme"), "dark")
 })
