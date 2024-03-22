241d5dcc8cc678d08ffd23a01a1ce9faff6f8ef6
MaxDac <massimiliano.dacunzo@hotmail.com>
2024-03-22 14:37:04 +0000

    Refactored notifications

--- apps/privee_web/assets/js/utils/push-notifications.mjs
+++ apps/privee_web/assets/js/utils/push-notifications.mjs
@@ -3,7 +3,7 @@
   * if it supports it asks for permission to the user and logs the result.
   */
 export const askNotificationPermission = () => {
-  const handlePermission = (_) => {
+  const handlePermission = () => {
     console.debug("Permission: ", Notification.permission === "granted" ? "granted" : "denied")
   }
   
@@ -15,42 +15,61 @@ export const askNotificationPermission = () => {
     console.debug("This browser does not support notifications.")
   }
 }
+
+/**
+  * Determines whether the chat window is currently on focus.
+  */
+const isWindowCurrentlyHidden = () => document.hidden
   
 /**
-    * Shows a notification with the given body and an optional title.
-    * @param {string} [body] - The body of the notification.
-    * @param {string} [title] - The title of the notification.
-    * @param {string} [url] - The url to open when the notification is clicked.
-    * @returns {Promise<?Notification>} - The notification.
-    */
-export const pushNotification = (body, title, url) => {
-  const imageUrl = "/favicon.ico"
-  const notificationTitle = title || " - Privee new notification"
+  * Shows a notification with the given body and an optional title.
+  * @param {string} [body] - The body of the notification.
+  * @param {string} [title] - The title of the notification.
+  * @param {string} [url] - The url to open when the notification is clicked.
+  * @param {boolean} [checkFocus] - Determines whether to check if the window is in focus 
+  * before sending the notification.
+  * @returns {Promise<?Notification>} - The notification.
+  */
+export const pushNotification = (body, title, url, checkFocus) => {
+  if (!checkFocus || isWindowCurrentlyHidden()) {
+    const imageUrl = "/favicon.ico"
+    const notificationTitle = title || " - Privee new notification"
 
-  const notification = new Notification(notificationTitle, {
-    body: body,
-    icon: imageUrl
-  })
+    const notification = new Notification(notificationTitle, {
+      body: body,
+      icon: imageUrl
+    })
 
-  // Open the chat when the notification is clicked.
-  notification.addEventListener("click", (_) => {
-    if (url) {
-      window.open(url, "_blank")
-    }
-  })
-    
-  return new Promise((resolve, _reject) => {
-    document.addEventListener("visibilitychange", (_) => {
-      if (document.visibilityState === "visible") {
-        resolve(notification)
+    // Open the chat when the notification is clicked.
+    notification.addEventListener("click", (_) => {
+      if (url) {
+        window.open(url, "_blank")
       }
     })
-  })
+      
+    return new Promise((resolve, _reject) => {
+      document.addEventListener("visibilitychange", (_) => {
+        if (document.visibilityState === "visible") {
+          resolve(notification)
+        }
+      })
+    })
+  } else {
+    return Promise.resolve(undefined)
+  }
 }
 
 /**
+  * @typedef {Object} EventDetails Represents the details of the event sent from the back end. For more information
+  * consult `events.ex` file.
+  * @property {string} [text] The text of the message that triggered the notification.
+  * @property {string} [session_name] The session name that sent the message.
+  * @property {boolean} [check_focus] Whether to check if the window is in focus before triggering the notification.
+  */
+
+/**
   * @typedef {Object & Event} PhoenixEvent This type represents a custom Phoenix event.
-  * @property {any} detail The event details
+  * @property {EventDetails} detail The event details
   */
 
 /**
@@ -58,6 +77,6 @@ export const pushNotification = (body, title, url) => {
  * @param {PhoenixEvent} event The event triggered from the back-end.
  */
 export const phoenixPushEventHandler = (event) => {
-  pushNotification(event.detail.text, "Privee - Text received", `/chat/${event.detail.session_name}`)
+  pushNotification(event.detail.text, "Privee - Text received", `/chat/${event.detail.session_name}`, event.detail.check_focus)
     .catch(e => console.error("Error showing notification: ", e))
 }
