0e961386e4c3581f7d4f95e8d5ed25ddc5f55d7b
MaxDac <massimiliano.dacunzo@hotmail.com>
2024-11-27 18:44:10 +0000

    Fixed `Gettext` warning

--- apps/privee_web/lib/privee_web/gettext.ex
+++ apps/privee_web/lib/privee_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule PriveeWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :privee_web
+  use Gettext.Backend, otp_app: :privee_web
 end
