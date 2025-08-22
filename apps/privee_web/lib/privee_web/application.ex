4bf8521ddaf3124bcf71539c1edd3c89669f9a4c
MaxDac <massimiliano.dacunzo@hotmail.com>
2025-08-22 09:43:00 +0100

    Reverting configuration

--- apps/privee_web/lib/privee_web/application.ex
+++ apps/privee_web/lib/privee_web/application.ex
@@ -16,7 +16,6 @@ defmodule PriveeWeb.Application do
       # Start to serve requests, typically the last entry
       # ,
       PriveeWeb.Endpoint,
-      {Cluster.Supervisor, [get_topologies(), [name: PriveeWeb.ClusterSupervisor]]},
       Chats
     ]
 
@@ -26,54 +25,11 @@ defmodule PriveeWeb.Application do
     Supervisor.start_link(children, opts)
   end
 
-  # Commented away until the Kubernetes deployment will be ready.
-  # defp get_topologies do
-  #   is_prod? = System.get_env("MIX_ENV")
-
-  #   strategy =
-  #     if is_prod? do
-  #       Cluster.Strategy.Kubernetes.DNS
-  #     else
-  #       Cluster.Strategy.Gossip
-  #     end
-
-  #   [
-  #     privee: [
-  #       strategy: strategy,
-  #       config: [
-  #         service: "privee-app-svc-headless",
-  #         application_name: "privee"
-  #       ]
-  #     ]
-  #   ]
-  # end
-
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
   @impl true
   def config_change(changed, _new, removed) do
     PriveeWeb.Endpoint.config_change(changed, removed)
     :ok
-  end
-
-  defp get_topologies do
-    is_prod? = System.get_env("MIX_ENV") == "prod"
-
-    strategy =
-      if is_prod? do
-        Cluster.Strategy.Kubernetes.DNS
-      else
-        Cluster.Strategy.Gossip
-      end
-
-    [
-      privee: [
-        strategy: strategy,
-        config: [
-          service: "privee-app-svc-headless",
-          application_name: "privee"
-        ]
-      ]
-    ]
   end
 end
