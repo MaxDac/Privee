fe7983eba1ac201ce834772fb9d407ba084af7ee
MaxDac <massimiliano.dacunzo@hotmail.com>
2025-08-30 13:13:04 +0100

    Fixing styles

--- apps/privee_web/lib/privee_web/components/core_components.ex
+++ apps/privee_web/lib/privee_web/components/core_components.ex
@@ -239,8 +239,8 @@ defmodule PriveeWeb.CoreComponents do
         "phx-submit-loading:opacity-75 py-2 px-3",
         "text-green-700 hover:text-white border border-green-700",
         "hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300",
-        "font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2", 
-        "dark:border-green-500 dark:text-green-500 dark:hover:text-white", 
+        "font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2",
+        "dark:border-green-500 dark:text-green-500 dark:hover:text-white",
         "dark:hover:bg-green-600 dark:focus:ring-green-800 cursor-pointer",
         @class
       ]}
@@ -271,7 +271,6 @@ defmodule PriveeWeb.CoreComponents do
       type={@type}
       class={[
         "phx-submit-loading:opacity-75 py-2 px-3",
-        "phx-submit-loading:opacity-75",
         "text-black focus:ring-4 focus:outline-none ",
         "font-medium rounded-lg text-sm text-center me-2 mb-2",
         @class
@@ -308,8 +307,8 @@ defmodule PriveeWeb.CoreComponents do
         "phx-submit-loading:opacity-75 py-2 px-3",
         "text-green-700 hover:text-white border border-green-700",
         "hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300",
-        "font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2", 
-        "dark:border-green-500 dark:text-green-500 dark:hover:text-white", 
+        "font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2",
+        "dark:border-green-500 dark:text-green-500 dark:hover:text-white",
         "dark:hover:bg-green-600 dark:focus:ring-green-800",
         @class
       ]}
