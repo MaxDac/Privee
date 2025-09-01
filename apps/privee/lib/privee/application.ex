7c35a609670352ad349be82f347befc0c9705263
MaxDac <massimiliano.dacunzo@hotmail.com>
2025-09-01 18:35:25 +0100

    Formatted

--- apps/privee/lib/privee/application.ex
+++ apps/privee/lib/privee/application.ex
@@ -15,8 +15,7 @@ defmodule Privee.Application do
     children = [
       Privee.Repo,
       {DNSCluster,
-       query: Application.get_env(:privee, :dns_cluster_query) || :ignore,
-       log: :info},
+       query: Application.get_env(:privee, :dns_cluster_query) || :ignore, log: :info},
       {Phoenix.PubSub, name: Privee.PubSub}
       # Start a worker by calling: Privee.Worker.start_link(arg)
       # {Privee.Worker, arg}
