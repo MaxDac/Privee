7b1e2ba22a7039f4d1b9d99e4ed6686c1b72bfc2
MaxDac <25101798+MaxDac@users.noreply.github.com>
2025-09-18 08:20:33 +0100

    Temp

--- apps/privee_web/assets/js/app.js
+++ apps/privee_web/assets/js/app.js
@@ -32,11 +32,13 @@ import { addChatHooks } from "./hooks/chat-hooks.mjs"
 import { addRegistrationHooks } from "./hooks/registration-hooks.mjs"
 import { exportDebugFunctions } from "./utils/debug.mjs"
 import { addBackEndEventHandlers } from "./hooks/event-handlers.mjs"
+import { addPriveeSelectorHooks } from "./hooks/privee-selector-hooks.mjs"
 
 // Setting up LiveView hooks
 const Hooks = {}
 addRegistrationHooks(Hooks)
 addChatHooks(Hooks)
+addPriveeSelectorHooks(Hooks)
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
---
+++ apps/privee_web/assets/js/hooks/privee-selector-hooks.mjs
@@ -0,0 +1,7 @@
+/**
+ * Adds hooks to the privee selector screen.
+ * @param {any} Hooks LiveView Hooks
+ */
+export function addPriveeSelectorHooks(Hooks) {
+  Hooks.PriveeSelectorScreen = {}
+}
